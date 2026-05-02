b082b42412b705e51aff7390bb59bece3d575553
felipem554 <m655321554@gmail.com>
2026-05-02 17:05:58 +0200

    refactor: replace mixed @Autowired/field injection with constructor injection
    - SecretMessageService: make fields final, drop unused PasswordGenerator dep
    - NatsService: make all collaborator fields final, remove @Autowired, fix
      typos (startNatsSubscripions, createDispacher), drop unused imports
    - SecretMessageIdentifier: remove redundant @Getter (covered by @Data)
    - SecretMessageAppApplication: remove redundant @ComponentScan (caused test
      inner TestConfig to be picked up by production component scan)

--- src/main/java/com/secret_message/secret_message_app/SecretMessageAppApplication.java
+++ src/main/java/com/secret_message/secret_message_app/SecretMessageAppApplication.java
@@ -2,10 +2,8 @@ package com.secret_message.secret_message_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.secret_message.secret_message_app")
 public class SecretMessageAppApplication {
 
 	public static void main(String[] args) {
